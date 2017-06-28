0605170f343077ea9c2b78b48d39d3c1621fc6a7
Zsp <943481660@qq.com>
2017-06-28 21:17:49 +0800

    确认账户

--- app/auth/views.py
+++ app/auth/views.py
@@ -14,9 +14,9 @@ def before_request():
 
 @auth.route('/unconfirmed')
 def unconfirmed():
-    if current_user.is_anonymous() or current_user.confirmed:
+    if current_user.is_anonymous or current_user.confirmed:
         return redirect(url_for('main.index'))
-    return render_template('auth.unconfirmed.html')
+    return render_template('auth/unconfirmed.html')
 
 @auth.route('/confirm')
 @login_required
