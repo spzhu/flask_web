d0d5331e54a59b916d7a8349c9c8fa5e1ebe7421
Zsp <943481660@qq.com>
2017-06-30 18:59:54 +0800

    用户资料页面

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, session, redirect, url_for, current_app
+from flask import render_template, session, redirect, url_for, current_app, abort
 from .. import db
 from ..models import User, Permission
 from ..decorators import admin_required, permission_required
@@ -10,22 +10,15 @@ from .forms import NameForm
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
- #   form = NameForm()
- #   if form.validate_on_submit():
- #       user = User.query.filter_by(username=form.name.data).first()
- #       if user is None:
- #           user = User(username=form.name.data)
- #           db.session.add(user)
- #           session['known'] = False
- #           if current_app.config['FLASKY_ADMIN']:
- #               send_email(current_app.config['FLASKY_ADMIN'], 'New User',
- #                          'mail/new_user', user=user)
- #       else:
- #           session['known'] = True
- #       session['name'] = form.name.data
- #       return redirect(url_for('.index'))
     return render_template('index.html', name=session.get('name'), \
                            known=session.get('known', False))
+
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
+    return render_template('user.html', user=user)
 
 @main.route('/admin')
 @login_required
